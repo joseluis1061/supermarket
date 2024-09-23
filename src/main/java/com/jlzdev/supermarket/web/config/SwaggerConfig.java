dc992136af022d7445266bb18851e81a7fe26e29
José Luis Zúñiga <jose@tangibledesign.co>
2024-09-23 13:24:28 -0500

    Add configuration by deploy

--- src/main/java/com/jlzdev/supermarket/web/config/SwaggerConfig.java
+++ src/main/java/com/jlzdev/supermarket/web/config/SwaggerConfig.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 public class SwaggerConfig {
   public static void main(String[] arg){
     SpringApplication.run(SwaggerConfig.class);
